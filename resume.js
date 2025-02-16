343299a82e7cdc610969922efbbff4fc9d493a46
nicoleunguyen <nicoleunguyen04@gmail.com>
2025-02-15 23:18:27 -0800

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,121 @@
+const RESUME_DATA = {
+    given: "Hi, I'm",
+    family: "Nicole! ₍ ᐢ.ˬ.ᐢ₎",
+    initials: "NN",
+    favicon: "./media/icons/favicon.ico",
+    avatarUrl: "https://avatars.githubusercontent.com/u/198544219?v=4",
+    personalWebsiteUrl: "nicolenguyen@github.io",
+    location: "san francisco bay area, ca",
+    locationLink: "https://www.google.com/maps/place/San+Francisco+Bay+Area,+CA",
+    title: "BS symbolic systems @ stanford university",
+    about: "B.S. symbolic systems @ stanford university <br> concentration in media and communication <br> minor in computer science",
+    summary: {
+      first: `I'm a community college transfer student at Stanford University. Prior to Stanford, I attended Foothill College and received my Associate's Degree in Sociology and Certificates of Achievement in Data Analytics and Humanities.`,
+      second: `My interests lie in the ways we create, interpret, and interact with information—whether through language, technology, or social systems. I'm passionate about the intersection of communication and computation, where media, marketing, and digital platforms shape how we connect and influence.`,
+      third: `Feel free to reach out!`
+    },
+    contact: {
+      email: "nicolenguyen@stanford.edu",
+      social: [
+        {
+          name: "LinkedIn",
+          url: "https://www.linkedin.com/in/nicoleunguyen/",
+          icon: "./media/icons/linkedin.svg"
+        },        
+      ],
+    },
+    work: [
+      {
+        company: "TomKat Center for Sustainability",
+        link: "https://tomkat.stanford.edu/",
+        contract: "Part-time",
+        title: "Student Assistant",
+        // logo: ParabolLogo,
+        start: "November 2024",
+        end: "Present",
+        description:
+          `Boosted applications to 2025 summer internship programs by 30% through curated posts, testimonials, former intern highlights, and targeted outreach across social media platforms and the center website.
+          Partnered with 70+ student organizations to promote sustainability-focused fellowship and internship opportunities, connecting students with Silicon Valley startups.`,
+      },
+      {
+        company: "Foundation for California Community Colleges",
+        link: "https://foundationccc.org/",
+        contract: "Internship",
+        title: "Communications & Content Marketing Intern",
+        // logo: ClevertechLogo,
+        start: "May 2024",
+        end: "August 2024",
+        description:
+          "Developed two monthly content calendars, leading to a 15% increase in social media reach across platforms. Updated and maintained website content using WordPress and Drupal, ensuring accuracy in resource listings and optimizing layout for readability. Enhanced brand consistency across digital platforms by aligning social media content, email campaigns, and event materials with FoundationCCCs mission, boosting audience engagement by 10%.",
+      },
+      {
+        company: "Stanford Institute for Research in the Social Sciences",
+        link: "https://iriss.stanford.edu/",
+        contract: "Internship",
+        title: "Research Assistant",
+        // logo: JojoMobileLogo,
+        start: "June 2023",
+        end: "June 2024",
+        description:
+          "Conducted thematic coding and comparative analysis using NVivo and RStudio to identify global trends in higher education access for Indigenous groups. Analyzed data from 60+ academic articles across 20 countries, shaping education policy recommendations. Recognized as 2024 Intern of the Year out of 40+ research interns.",
+      },
+    ],
+    skills: [
+      "Social Media Optimizaton",
+      "Copy Editing",
+      "Wordpress",
+      "Google Analytics",
+      "Tableau",
+      "Content Creation",
+      "Trend & Growth Analysis",
+      "Adobe Creative Suite",
+      "Final Cut Pro",
+      "Drupal",
+      "HTML",
+      "SQL",
+      "Python",
+      "C++ ",
+      "R",
+    ],
+    projects: [
+        {
+            title: "Nursing Education Grant Press Release",
+            tags: [
+              "Copy Editing",
+              "Storytelling",
+              "Content Writing",
+            ],
+            description: "Developed newsletter using information synthesis to highlight key grant details.",
+            link: {
+              label: "FoundationCCC",
+              href: "https://foundationccc.org/nursing-education-investment-grants-program-awards-over-450000-to-improve-nursing-education-through-california-community-colleges/",
+            },
+            logo: "./media/projects/nursinggrant.png",
+            start: "June 2024 ",
+          },
+      {
+        title: "Grant Funding and Workforce Development Press Release",
+        tags: ["Copy Editing", "Storytelling", "Content Writing",
+        ],
+        description:
+          "Wrote a newsletter highlighting 4.2M in workforce development grants and their impact on employment access.",
+        logo: "./media/projects/workforce.png",
+        link: {
+          label: "FoundationCCC",
+          href: "https://foundationccc.org/foundation-for-california-community-colleges-and-state-workforce-board-award-4-2m-to-develop-essential-workforce-skills/",
+        },
+        start: "May 2024",
+    }
+    ],
+    students: [
+      {given: "Yélognissè", family: "Agbohessou"},
+      {given: "Alexis",family: "Bonnet"},
+      {given: "Oriane", family: "Braud"},
+      {given: "Moussa",family: "Diedhiou"},
+      {given: "Mathilde",family: "Millan"},
+      {given: "Valentin",family: "Torrelli"},
+      {given: "Simon",family: "Treillou"},
+      {given: "Sidy", family: "Sow"},
+      {given: "Thomas", family: "Wibaux"},
+    ],
+};
