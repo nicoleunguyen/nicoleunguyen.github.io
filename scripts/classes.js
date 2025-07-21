0cce950d62131c887972790c16d63c5cd9d4013d
nicoleunguyen <nicoleunguyen04@gmail.com>
2025-07-21 09:44:03 -0700

    Update classes.js

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,9 +1,9 @@
-const CLASSES_DATA = RESUME_DATA.skills.map((className) => {
+const COURSES_DATA = RESUME_DATA.relevant_courses.map((course) => {
     return `
     <div class="inline-flex items-center rounded-md border px-2 py-0.5 text-xs font-semibold font-mono transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-nowrap border-transparent bg-primary/80 text-primary-foreground hover:bg-primary/60">
-        ${className}
+        ${course}
     </div>
     `;
 }).join("");
 
-document.querySelector(".js-classes").innerHTML += CLASSES_DATA;
+document.querySelector(".js-relevant_courses").innerHTML += COURSES_DATA;
