7de60d3ca9bf628dfab7c889a9614e0c3e83678f
nicoleunguyen <nicoleunguyen04@gmail.com>
2025-07-21 11:50:11 -0700

    Update courses.js

--- scripts/courses.js
+++ scripts/courses.js
@@ -1,8 +1,8 @@
 const COURSES_DATA = RESUME_DATA.relevant_courses.map((course) => {
     return `
-    <div class="inline-flex items-center rounded-md border px-2 py-0.5 text-xs font-semibold font-mono transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-nowrap border-transparent bg-primary/80 text-primary-foreground hover:bg-primary/60">
-        ${course}
-    </div>
+    <a href="${course.link}" target="_blank" rel="noopener noreferrer" class="inline-flex items-center rounded-md border px-2 py-0.5 text-xs font-semibold font-mono transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-nowrap border-transparent bg-primary/80 text-primary-foreground hover:bg-primary/60">
+        ${course.title}
+    </a>
     `;
 }).join("");
 
